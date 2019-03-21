742586439022aec45cedab11430504bda57c30fc
valentinanyaz <valya.kn.work@gmail.com>
2019-03-21 11:19:57 +0300

    finished clicker

--- app/src/main/java/com/example/startgit/MainActivity.java
+++ app/src/main/java/com/example/startgit/MainActivity.java
@@ -2,17 +2,29 @@ package com.example.startgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btn;
+    int countClicks = 0;
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.button);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countClicks++;
+                textView.setText(Integer.toString(countClicks));
+            }
+        });
     }
 }
